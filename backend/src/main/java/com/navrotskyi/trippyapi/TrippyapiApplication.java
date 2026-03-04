3df07fac3c1009bd4420beab8339b86f2705cc94
MarshallBjorn <bjornstudio228@gmail.com>
2026-03-04 13:59:53 +0100

    Feature: filled docker-compose file, initialized Spring Boot(Without hibernate)

---
+++ backend/src/main/java/com/navrotskyi/trippyapi/TrippyapiApplication.java
@@ -0,0 +1,13 @@
+package com.navrotskyi.trippyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippyapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/navrotskyi/trippyapi/TrippyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navrotskyi.trippyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
