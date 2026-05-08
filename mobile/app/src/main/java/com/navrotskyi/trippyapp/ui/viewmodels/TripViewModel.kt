fb9dc265b1ed1bf4de1cbbfb0d79460a9924f73f
Michał Domański <abzi.mnut@gmail.com>
2026-05-08 07:33:51 +0200

    1.1/2 tego by tylko organizer mogl zapraszac a nie kazdy

--- mobile/app/src/main/java/com/navrotskyi/trippyapp/ui/viewmodels/TripViewModel.kt
+++ mobile/app/src/main/java/com/navrotskyi/trippyapp/ui/viewmodels/TripViewModel.kt
@@ -116,6 +116,7 @@ class TripViewModel : ViewModel() {
                     _trips.value = response.body()!!.map { dto ->
                         Trip(
                             id = dto.id,
+                            ownerId = dto.ownerId,
                             owner = null,
                             name = dto.name,
                             pickedCurrency = dto.currencyCode,
