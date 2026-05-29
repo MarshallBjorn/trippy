1bb5546cb289985162385931b0ec4b08095b0498
MarshallBjorn <bjornstudio228@gmail.com>
2026-05-29 13:15:56 +0200

    feat: Revised security concerns

--- backend/src/main/java/com/navrotskyi/trippyapi/domain/TokenBlacklist.java
+++ backend/src/main/java/com/navrotskyi/trippyapi/domain/TokenBlacklist.java
@@ -3,12 +3,16 @@ package com.navrotskyi.trippyapi.domain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.Instant;
 
-@Data
+@Getter
+@Setter
+@ToString(exclude = "token")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
